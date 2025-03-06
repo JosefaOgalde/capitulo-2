2478f1252b7fe47ce78253214e6762ab4572b0a7
JosefaOgalde <105354294+JosefaOgalde@users.noreply.github.com>
2025-03-06 16:52:17 -0300

    Create TiposDeDatosPrimitivos.java

---
+++ TiposDeDatosPrimitivos.java
@@ -0,0 +1,15 @@
+public class TiposDeDatosPrimitivos {
+    public static void main(String[] args) {
+        byte elMenorByte = Byte.MIN_VALUE;
+        byte elMayorByte = Byte.MAX_VALUE;
+        System.out.println("El tipo byte es un número entero de 8 bits con signo. Está comprendido entre " + elMenorByte + " y " + elMayorByte);
+
+        System.out.println("El tipo byte es un número entero de 8 bits con signo. Está comprendido entre " + Short.MIN_VALUE + " y " + Short.MAX_VALUE);
+
+        System.out.println("El tipo byte es un número entero de 8 bits con signo. Está comprendido entre " + Integer.MIN_VALUE + " y " + Integer.MAX_VALUE);
+
+        System.out.println("El tipo byte es un número entero de 8 bits con signo. Está comprendido entre " + Long.MIN_VALUE + " y " + Long.MAX_VALUE);
+
+    }
+
+}
